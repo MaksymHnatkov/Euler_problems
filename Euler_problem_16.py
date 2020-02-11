7c823573bebb7e688637b8c39abe53626a1eb9e3
Maksym Hnatkov <mgnatkov@gmail.com>
2020-02-12 00:58:43 +0200

    Euler problem #2 solution

---
+++ Euler_problem_16.py
@@ -0,0 +1,10 @@
+b = 2**1000
+a = list(str(b))
+sum1 = 0
+for i in a:
+    i = int(i)
+    sum1 = sum1 + i
+
+print(sum1)
+
+
---
+++ Euler_problem_2.py
@@ -0,0 +1,21 @@
+# Euler problem 2:
+
+a = []
+i = 1
+s = 0
+
+while (i + s) < 4000000:
+    s += i
+    i += s
+    a.append(s)
+    a.append(i) 
+
+total_sum = 0
+for i in a:
+    if i % 2 == 0:
+        total_sum = total_sum + i
+
+
+print(total_sum)
+
+
