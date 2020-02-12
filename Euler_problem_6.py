6cac8a7b7cc3a594aed4f085ee52128769489352
Maksym Hnatkov <mgnatkov@gmail.com>
2020-02-12 22:53:47 +0200

    Euler problem #6 solution

--- Euler_problem_6.py
+++ Euler_problem_6.py
@@ -1,2 +1,16 @@
 # Euler problem 6:
 
+sumSqrt = 0
+sqrtSum = 0
+
+for i in range (1,101):
+    sumSqrt += i**2
+
+for z in range (1,101):
+    sqrtSum += z
+
+sqrtSum = sqrtSum ** 2
+
+answer = sqrtSum - sumSqrt
+
+print(answer)
