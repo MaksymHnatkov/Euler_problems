5031a87d62b3401641e44d6abc8fb51ec997a95c
Maksym Hnatkov <mgnatkov@gmail.com>
2020-02-13 15:57:48 +0200

    Euler problem #7 solution (no func).

--- Euler_problem_7.py
+++ Euler_problem_7.py
@@ -1,16 +1,15 @@
 # Euler problem 7:
 
-def primes():
-    n = 10001
-    x = 2
-    lst = []
-    while (len(lst) < n):
-        if all(x % i != 0 for i in lst):
-            lst.append(x)
-        x += 1
-    print(lst[-1])
+n = 10
+x = 2
+lst = []
+while (len(lst) < n):
+    if all(x % i != 0 for i in lst):
+        lst.append(x)
+    x += 1
+print(lst[-1])
 
-primes()
+
 
 
 
